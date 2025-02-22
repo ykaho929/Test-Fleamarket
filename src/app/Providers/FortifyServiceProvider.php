f2cef7fda8ce960c9edd14be30950bb9d361c09f
ykaho <h12.929.kaho@gmail.com>
2025-02-22 21:03:24 +0900

    ER図ログイン認証修正

--- src/app/Providers/FortifyServiceProvider.php
+++ src/app/Providers/FortifyServiceProvider.php
@@ -29,18 +29,19 @@ class FortifyServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Fortify::createUsersUsing(CreateNewUser::class);
-        Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
-        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
-        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+        
+        Fortify::registerView(function () {
+            return view('auth.register');
+        });
+
+        Fortify::loginView(function () {
+            return view('auth.login');
+        });
 
         RateLimiter::for('login', function (Request $request) {
-            $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())).'|'.$request->ip());
-
-            return Limit::perMinute(5)->by($throttleKey);
-        });
-
-        RateLimiter::for('two-factor', function (Request $request) {
-            return Limit::perMinute(5)->by($request->session()->get('login.id'));
+            $email = (string) $request->email;
+            return Limit::perMinute(10)->by($email . $request->ip());
         });
     }
+    
 }
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -13,18 +13,21 @@
         <div class="header__inner">
             <div class="header-utilities">
                 <a class="header__logo" href="/">
-                    Attendance Management
+                    COACHTECH
                 </a>
                 <nav>
                     <ul class="header-nav">
+                         @if (Auth::check())
                         <li class="header-nav__item">
                             <a class="header-nav__link" href="/mypage">マイページ</a>
                         </li>
                         <li class="header-nav__item">
-                        <form>
-                            <button class="header-nav__button">ログアウト</button>
-                        </form>
+                            <form class="form" action="/logout" method="post">
+                                @csrf
+                                <button class="header-nav__button">ログアウト</button>
+                            </form>
                         </li>
+                        @endif
                     </ul>
                 </nav>
             </div>
