19858d4d0149bfbfb27ea601a22165ce8bb8c97b
ykaho <h12.929.kaho@gmail.com>
2025-03-11 16:39:29 +0900

    プロフィール編集画面たたき

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function favorite(){
+        return view('user');
+    }
+
+    public function register(){
+        return view('profile');
+    }
+
     
 }
