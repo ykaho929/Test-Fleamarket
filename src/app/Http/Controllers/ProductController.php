55332fb20a432ee7695a1acd06dcbdc01e62adf4
ykaho <h12.929.kaho@gmail.com>
2025-03-23 08:25:57 +0900

    商品購入ページ追加

--- src/app/Http/Controllers/ProductController.php
+++ src/app/Http/Controllers/ProductController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function sell (){
-        return veiw (sell)
+    public function sell(){
+        return view ('sell');
     }
 }
--- src/resources/views/mypage.blade.php
+++ src/resources/views/mypage.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/order.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/mypage.css') }}">
 @endsection
 
 @section('content')
