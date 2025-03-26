8b2aa05cf0eb28371bfb15427995f6b72fefc462
ykaho <h12.929.kaho@gmail.com>
2025-03-26 23:09:12 +0900

    マイページたたき

--- src/resources/views/mypage.blade.php
+++ src/resources/views/mypage.blade.php
@@ -5,3 +5,29 @@
 @endsection
 
 @section('content')
+    <div class="mypage__content">
+        <div class="mypage__tabs">
+            <a href="#redommend">おすすめ</a>
+            <a href="#mylist">マイリスト</a>
+        </div>
+
+        <div class="mypage__inner">
+            <div class="recommend__inner">
+                <div id="favorites" class="tab-content {{ request()->routeIs('mypage') && !request()->has('selling') ? '' : '' }}">
+                <h2>お気に入り商品</h2>
+                @forelse ($favorites as $favorite)
+                    
+                    <p>{{ $favorite->product->name }}</p>
+                @endforelse
+            </div>
+            <div class="recommend__inner">
+                <div id="favorites" class="tab-content {{ request()->routeIs('mypage') && !request()->has('selling') ? '' : '' }}">
+                <h2>マイリスト</h2>
+                @forelse ($favorites as $favorite)
+                    
+                    <p>{{ $favorite->product->name }}</p>
+                @endforelse
+            </div>
+        </div>
+</div>
+@endsection
